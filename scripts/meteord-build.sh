61e8512139794788f110e0554b2eef231a39338e
Jake Gaylor <jhgaylor@gmail.com>
2015-03-03 07:34:39 -0600

    Update meteord-build.sh

--- scripts/meteord-build.sh
+++ scripts/meteord-build.sh
@@ -3,14 +3,14 @@ curl https://install.meteor.com | /bin/sh
 cd /app
 ls
 pwd
-#meteor build --directory /tmp/the-app
+meteor build --directory /tmp/the-app
 
-#cd /tmp/the-app/bundle/programs/server/
-#npm i
+cd /tmp/the-app/bundle/programs/server/
+npm i
 
-#mv /tmp/the-app/bundle /built_app
+mv /tmp/the-app/bundle /built_app
 
 # cleanup
-#rm -rf /tmp/the-app
-#rm -rf ~/.meteor
-#rm /usr/local/bin/meteor
+rm -rf /tmp/the-app
+rm -rf ~/.meteor
+rm /usr/local/bin/meteor
