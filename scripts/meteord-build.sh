8a98abe7ed965f683a6c29d2040725a84eb1f582
Jake Gaylor <jhgaylor@gmail.com>
2015-03-04 09:32:31 -0600

    Update meteord-build.sh

--- scripts/meteord-build.sh
+++ scripts/meteord-build.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 curl https://install.meteor.com | /bin/sh
-npm install --silent -g demeteorizer
+
 cd /app
+meteor build --directory /tmp/the-app
 
-# meteor build --directory /tmp/the-app
-demeteorizer --output /tmp/the-app
-
-
-# cd /tmp/the-app/bundle/programs/server/
-# npm i
-cd /tmp/the-app
+cd /tmp/the-app/bundle/programs/server/
 npm i
 
-# mv /tmp/the-app/bundle /built_app
-mv /tmp/the-app /built_app
+mv /tmp/the-app/bundle /built_app
 
 # cleanup
 rm -rf /tmp/the-app
