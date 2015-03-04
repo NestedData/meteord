444c61b186c626c201e66a57588e183c2d6c0788
Jake Gaylor <jhgaylor@gmail.com>
2015-03-04 07:14:22 -0600

    Update run_app.sh

--- scripts/run_app.sh
+++ scripts/run_app.sh
@@ -1,5 +1,28 @@
-cd /built_app
+set -e
 
-export PORT=3000
-echo "!!Now starting meteor app on port:$PORT"
+if [ -d /bundle ]; then
+  cd /bundle
+  tar xzf *.tar.gz
+  cd /bundle/bundle/programs/server/
+  npm i
+  cd /bundle/bundle/
+elif [[ $BUNDLE_URL ]]; then
+  cd /tmp
+  wget $BUNDLE_URL -O bundle.tar.gz
+  tar xzf bundle.tar.gz
+  cd /tmp/bundle/programs/server/
+  npm i
+  cd /tmp/bundle/
+else
+  cd /built_app
+fi
+
+if [[ $REBULD_NPM_MODULES ]]; then
+  cd programs/server
+  bash /opt/meteord/rebuild_npm_modules.sh
+  cd ../../
+fi
+
+export PORT=80
+echo "starting meteor app on port:$PORT"
 node main.js
