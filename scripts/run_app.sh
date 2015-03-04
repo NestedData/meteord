b935585979c3cd7c7327313b4e22501a682de4ef
Jake Gaylor <jhgaylor@gmail.com>
2015-03-04 06:35:54 -0600

    Update run_app.sh

--- scripts/run_app.sh
+++ scripts/run_app.sh
@@ -1,11 +1,5 @@
 cd /built_app
 
-if [[ $REBULD_NPM_MODULES ]]; then
-  cd programs/server
-  bash /opt/meteord/rebuild_npm_modules.sh
-  cd ../../
-fi
-
 export PORT=3000
 echo "!!Now starting meteor app on port:$PORT"
 node main.js
