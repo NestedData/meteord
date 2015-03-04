36e0958175d9c11c4cf3c3c8f7721cf0c874af28
Jake Gaylor <jhgaylor@gmail.com>
2015-03-04 05:53:19 -0600

    Update run_app.sh

--- scripts/run_app.sh
+++ scripts/run_app.sh
@@ -1,21 +1,4 @@
-set -e
-
-if [ -d /bundle ]; then
-  cd /bundle
-  tar xzf *.tar.gz
-  cd /bundle/bundle/programs/server/
-  npm i
-  cd /bundle/bundle/
-elif [[ $BUNDLE_URL ]]; then
-  cd /tmp
-  wget $BUNDLE_URL -O bundle.tar.gz
-  tar xzf bundle.tar.gz
-  cd /tmp/bundle/programs/server/
-  npm i
-  cd /tmp/bundle/
-else
-  cd /built_app
-fi
+cd /built_app
 
 if [[ $REBULD_NPM_MODULES ]]; then
   cd programs/server
@@ -24,5 +7,5 @@ if [[ $REBULD_NPM_MODULES ]]; then
 fi
 
 export PORT=3000
-echo "starting meteor app on port:$PORT"
+echo "Now starting meteor app on port:$PORT"
 node main.js
